e015bd91fb3657322cc037d5098e24046ae41da2
Mohamed Sallam <mohamed.sallam@learnit.fi>
2023-06-26 14:39:15 +0300

    Connect to mongodb

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+require('dotenv').config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is up and running on port 3000");
-});
-
 const PORT = 3000;
 
+app.listen(PORT, () => {
+  console.log(`Server is running and listening to PORT:  ${PORT}`);
+});
+
+
+// connect to mongodb
+const dbURI = process.env.DB_URI;
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) =>{
+    console.log('Connected to mongodb succesfully');
+  })
+  .catch((err) => console.log(err));
+
+
+
+//Routes
 app.get("/", (req, res) => {
-  res.send(`Server is running and listening to PORT:  ${PORT}`);
+  res.send("This is Home Page");
+});
+
+app.get("/plants-list", (req, res) => {
+  res.send("This is Plants List Page");
+});
+
+app.get("/plant-details", (req, res) => {
+  res.send("This is Plant Details Page");
+});
+
+app.get("/my-garden", (req, res) => {
+  res.send("This is My Garden Page");
+});
+
+// Throw an error if the URL is not found
+app.get("/*", (req, res) => {
+  res.send("Oops, 404! Resources not found!!");
 });
