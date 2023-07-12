e852431b06c6e4f71d42ed4eb86f15736efe29d5
Mohamed Sallam <mohamed.sallam@learnit.fi>
2023-07-12 16:37:14 +0300

    Get Logged in user data from session api

--- src/pages/api/loggedinUser.js
+++ src/pages/api/loggedinUser.js
@@ -1,13 +1,10 @@
-import {getSession} from 'next-auth/react'
+import { getSession } from "next-auth/react";
 
-  export default async function handle (req, res) {
-    try {
-
-        const session = await getSession({req})
-            return res.status(200).json(session)
-   
-      
-    } catch (error) {
-      return res.status(500).json({ ...error, message: error.message })
-    }
+export default async function handle(req, res) {
+  try {
+    const session = await getSession({ req });
+    return res.status(200).json(session);
+  } catch (error) {
+    return res.status(500).json({ ...error, message: error.message });
   }
+}
