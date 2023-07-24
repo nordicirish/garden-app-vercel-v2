1780b87b10a0a971c76ad1975fc4d93a933f2484
Roger Graham <roger.graham@tuni.fi>
2023-07-24 14:12:53 +0300

    Add mapping for hosting env variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // map hosting env variables to nextjs env variables
+  env: {
+    NEXT_PUBLIC_NEXTAUTH_URL: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    SECRET: process.env.SECRET,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
